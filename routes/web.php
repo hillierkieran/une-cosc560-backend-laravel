5f412ab237c6be0e42b8ea3d77726b297cc738ee
Kieran Hillier <khillie2@myune.edu.au>
2024-08-14 15:56:14 +1000

    Updated composer + fixed "home" route name conflict

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Public routes
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('root');
 
 // Authentication routes
 Auth::routes();
